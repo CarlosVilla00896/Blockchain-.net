6c2a849290f6549104411c9d37f720e99ea84161
Carlos <carlosvilla00896@gmail.com>
2020-09-21 13:46:58 -0600

    Init commit

---
+++ BlockChain/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChain
+{
+    class Transaction
+    {
+        public string From { get; }
+        public string To { get; }
+        public double Amount { get; }
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+        
+    }
+}
